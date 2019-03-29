c66565c58af2afcde51db96747d59b0002fafa8e
Kritsana Wonglomnil <developer@gmail.com>
2019-03-29 22:11:16 +0700

    update backend

--- server/src/app.js
+++ server/src/app.js
@@ -1,40 +1,28 @@
-let express = require('express')
-let bodyParser = require('body-parser')
-const app = express()
-
+let express = require('express') 
+let bodyParser = require('body-parser') 
+ 
+const app = express() 
+ 
 app.use(bodyParser.json()) 
-app.use(bodyParser.urlencoded({extended: true})) 
-
-app.get('/status', function (req, res){
-    res.send('Hello nodejs server')
-})
-
-app.get('/hello/:person', function (req, res){
-    console.log('hello - ' + req.param.person)
-    res.send('Say hello with' + req.param.person)
-})
-
-app.get('/user/:userId', function (req, res){
-    res.send('ดูข้อมูลผู้ใช้งาน')
-})
-
-app.get('/users', function (req, res){
-    res.send('เรียกดูข้อมูลผู้ใช้งานทั้งหมด')
-})
-
-app.post('/user/', function (req, res) {   
-    res.send('ทำการสร้างผู้ใช้งาน: ' + JSON.stringify(req.body)) 
+app.use(bodyParser.urlencoded({
+    extended: true
+})) 
+ 
+require('./routes')(app) 
+ 
+app.get('/status', function (req, res ){   
+    res.send('Hello nodejs server') 
 }) 
  
-app.put('/user/:userId', function (req, res) {   
-    res.send('ทําการแก้ไขผุ้ใช้งาน: ' + req.params.userId + ' : ' +   JSON.stringify(req.body)) 
+app.get('/hello/:person', function (req,res) {   
+    console.log('hello - ' + req.params.person)   
+    res.send('sey hello with ' + req.params.person) 
 }) 
  
-app.delete('/user/:userId', function (req, res) {   
-    res.send('ทำการลบผู้ใช้งาน: ' + req.params.userId + ' : ' +    JSON.stringify(req.body)) 
-})    
-
-let port = 8081
-app.listen(port, function () {
-    console.log('server running on ' + port)
-})
+app.post('/hello', function (req, res) {   
+    res.send('OK  you post - ' + req.body.name) 
+}) 
+ 
+let port = 8081 
+ 
+app.listen(port, function () {   console.log('server running on ' + port) }) 
